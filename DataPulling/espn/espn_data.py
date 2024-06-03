17cd9aa1538d6fb7ef9aa295486a24bced88d1dc
Tarek Sallam <tarek@sallam.ca>
2024-06-03 12:01:42 -0400

    updated gitignore, added comments

--- DataPulling/espn/espn_data.py
+++ DataPulling/espn/espn_data.py
@@ -3,7 +3,8 @@ import json;
 
 url = "https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/2024/players?scoringPeriodId=0&view=players_wl";
 
-r = rq.get(url=url, headers={
+#ESPN api request for all players and their ids
+players = rq.get(url=url, headers={
     'Accept': 'application/json', 
     'Accept-Encoding': 'gzip, deflate, br', 
     'Connection': 'keep-alive',
@@ -14,7 +15,10 @@ r = rq.get(url=url, headers={
     'X-Fantasy-Platform': 'kona-PROD-aa413c7e338f6f98dbe8ea58e2d60e26ef8e3949',
     'X-Fantasy-Source': 'kona'
     });
-#print(json.dumps(dict(r.headers), indent = 4));
-str = json.dumps(r.json(), indent = 4);
+
+# convert to indented json
+players_str = json.dumps(players.json(), indent = 4);
+
+# write to json file
 with open("raw/players.json", 'w') as f:
-    f.write(str);
+    f.write(players_str);
