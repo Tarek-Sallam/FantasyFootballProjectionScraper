8aa382a133ad4476d11cbdef8f8e3317d86c9d9b
Tarek Sallam <tarek@sallam.ca>
2024-06-04 13:15:10 -0400

    updated espn pull to return a dataframe with data

--- espn_data_pull.py
+++ espn_data_pull.py
@@ -1,13 +1,40 @@
 import requests as rq
-import json;
-import os;
+import pandas as pd
 
-def pull_espn_data() -> None:
+def pull_espn_data() -> pd.DataFrame:
     url = "https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/2024/segments/0/leaguedefaults/3?view=kona_player_info";
     headers = {
             'X-Fantasy-Filter': '{"players":{"filterStatsForExternalIds":{"value":[2023,2024]},"filterSlotIds":{"value":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,23,24]},"filterStatsForSourceIds":{"value":[0,1]},"useFullProjectionTable":{"value":true},"sortAppliedStatTotal":{"sortAsc":false,"sortPriority":3,"value":"102024"},"sortDraftRanks":{"sortPriority":2,"sortAsc":true,"value":"PPR"},"sortPercOwned":{"sortPriority":4,"sortAsc":false},"limit":0,"filterRanksForSlotIds":{"value":[0,2,4,6,17,16,8,9,10,12,13,24,11,14,15]},"filterStatsForTopScoringPeriodIds":{"value":2,"additionalValue":["002024","102024","002023","022024"]}}}'
     }
+    names = []
+    pos = []
+    pts = []
+
     players = rq.get(url=url, headers = headers)
-    players_str = json.dumps(players.json(), indent=4);
-    with open(os.path.join(os.path.dirname(__file__), './uncleaned_data/' + 'espn.json'), 'w') as f:
-        f.write(players_str)
+    players_list = players.json()["players"];
+    names = [item["player"]["fullName"] for item in players_list]
+
+    for player in [item["player"]["defaultPositionId"] for item in players_list]:
+        if player == 2:
+            pos.append('RB')
+        elif player == 3:
+            pos.append('WR')
+        elif player == 4:
+            pos.append('TE')
+        elif player == 1:
+            pos.append('QB')
+        elif player == 5:
+            pos.append('K')
+        elif player == 16:
+            names[len(pos)] = names[len(pos)].split()[0]
+            pos.append('DEF')
+
+    #grab the projected point data
+    for player in [item["player"]["stats"] for item in players_list]:
+        if (len(player) == 2):
+            pts.append(player[1]["appliedTotal"])
+        else:
+            pts.append(player[2]["appliedTotal"])
+
+    
+    return(pd.DataFrame({"name": names, "position": pos, "proj": pts}))
--- nfl_data_pull.py
+++ nfl_data_pull.py
@@ -1,10 +1,11 @@
-#scrapes all nfl fantasy projections from fantasy.nfl.com
+#scrapes all nfl fantasy projections from fantasy.nfl.com and places into data frame
 
 #imports
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
-def pull_nfl_data():
+def pull_nfl_data() -> pd.DataFrame:
     # part 1 of the url (need to concatenate offset to get all players)
     urlpt1 = "https://fantasy.nfl.com/research/projections?offset="
 
@@ -14,6 +15,7 @@ def pull_nfl_data():
     # lists to fill with data
     names = []
     pts = []
+    pos = []
 
     # loop through the amount of pages
     for i in range(38):
@@ -28,10 +30,16 @@ def pull_nfl_data():
         table = soup.find('table')
         table_body = table.find('tbody')
         
-        # get the name elements and point elements
+        # get the name elements, point elements and position elements
         names_els = table_body.find_all('a', class_ = 'playerName')
+        pos_els = table_body.find_all('em')
         pts_els = table_body.find_all('td', class_ = 'projected')
         
-        # strip the names and points
+        # strip the names and points and positions
         names.extend([name.text for name in names_els])
         pts.extend([pts.text for pts in pts_els])
+        pos.extend([pos.text.split()[0] for pos in pos_els])
+
+    # return a dataframe with the data
+    return pd.DataFrame({'name': names, 'position': pos, 'proj': pts})
+pull_nfl_data()
